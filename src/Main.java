79de50ed341f76eb34a2dc8af81d4e7e51fb2538
alex-ganchev <alexander.m.ganchev@gmail.com>
2023-06-12 23:13:41 +0300

    Bulls and Cows - change in the way the number is generated

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,41 @@
+import java.util.HashSet;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    public static int[] generateRandomNumber(){
-        int[] number = new int[4];
+    public static int[] generateRandomNumber(int digits){
+        int[] number = new int[digits];
+        int i = 0;
         Random rnd = new Random();
-        while (true) {
-            for (int i = 0; i < number.length; i++) {
-                number[i] = rnd.nextInt(10);
-            }
-            if (number[0] != 0 && number[0] != number[1] && number[0] != number[2] && number[0] != number[3] &&
-                    number[1] != number[2] && number[1] != number[3] && number[2] != number[3]){
-                break;
-            }
+        HashSet<Integer> uniqeNumbers = new HashSet<>();
+        while (uniqeNumbers.size() < digits) {
+            uniqeNumbers.add(rnd.nextInt(10));
+        }
+        for (int element: uniqeNumbers) {
+            number[i++] = element;
         }
         return number;
     }
-    public static int[] checkCowsAndBulls(int num, int[] number){
+    public static int[] checkCowsAndBulls(int inputNumber, int[] randomNumber){
         int countCows = 0;
         int countBulls = 0;
-        int[] inputNumber = new int[number.length];
+        int[] arrayInputNumber = new int[randomNumber.length];
         int[] cowsBulls = new int[2];
-        int k = number.length-1;
-        while (num > 0){
-            int temp = num % 10;
+        int k = randomNumber.length-1;
+        while (inputNumber > 0){
+            int temp = inputNumber % 10;
             //System.out.println(temp);
-            inputNumber[k] = temp;
-            num = num / 10;
+            arrayInputNumber[k] = temp;
+            inputNumber = inputNumber / 10;
             k--;
         }
-        for (int i = 0; i < number.length; i++) {
-            for (int j = 0; j < inputNumber.length; j++) {
-                if (inputNumber[j] == number[i] && j != i) {
+        for (int i = 0; i < randomNumber.length; i++) {
+            for (int j = 0; j < arrayInputNumber.length; j++) {
+                if (arrayInputNumber[j] == randomNumber[i] && j != i) {
                     countCows++;
                 }
             }
-            if (inputNumber[i] == number[i]) {
+            if (arrayInputNumber[i] == randomNumber[i]) {
                 countBulls++;
             }
         }
@@ -43,7 +43,7 @@ public class Main {
         cowsBulls[1] = countBulls;
         return cowsBulls;
     }
-    
+
     public static void printArray(int[] array){
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i]);
@@ -51,13 +51,15 @@ public class Main {
     }
     public static void main(String[] args) {
         Scanner scanner  = new Scanner(System.in);
-        int[] randomNumber = generateRandomNumber();
+        int[] randomNumber = generateRandomNumber(4);
         int[] cowsBulls = new int[2];
+        printArray(randomNumber);
+        System.out.println();
         while (cowsBulls[1] != 4) {
-            System.out.print("Въведете число: ");
-            int n = scanner.nextInt();
-            cowsBulls = checkCowsAndBulls(n, randomNumber);
-            System.out.println(n + " - " + cowsBulls[0] + (cowsBulls[0] == 1 ? " крава" : " крави") + " и " + cowsBulls[1] + (cowsBulls[1] == 1 ? " бик" : " бика"));
+        System.out.print("Въведете число: ");
+        int inputNumber = scanner.nextInt();
+        cowsBulls = checkCowsAndBulls(inputNumber, randomNumber);
+        System.out.println(inputNumber + " - " + cowsBulls[0] + (cowsBulls[0] == 1 ? " крава" : " крави") + " и " + cowsBulls[1] + (cowsBulls[1] == 1 ? " бик" : " бика"));
         }
     }
 }
