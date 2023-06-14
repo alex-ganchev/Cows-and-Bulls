6044eb6b6511cd038e6b64c910710760f92a6266
alex-ganchev <alexander.m.ganchev@gmail.com>
2023-06-14 20:33:52 +0300

    Bulls and Cows - clear code and better error reporting

--- src/Main.java
+++ src/Main.java
@@ -21,13 +21,26 @@ public class Main {
         try {
             Integer.parseInt(inputNumber);
         } catch (NumberFormatException e) {
+            System.out.println("Въведохте текст. Моля въведете число с " + digits + (digits == 1 ? " цифрa." : " цифри."));
+
+            return false;
         }
         if (inputNumber.length() != digits) {
+            System.out.print("Въведохте число с " + inputNumber.length() + (inputNumber.length() == 1 ? " цифрa." : " цифри. "));
+            System.out.println("Моля въведете число с " + digits + (digits == 1 ? " цифрa." : " цифри."));
+
+            return false;
+        }
+        if (inputNumber.charAt(0) == '0') {
+            System.out.println("Моля въведете число без водеща 0 и с " + digits + (digits == 1 ? " цифрa." : " цифри."));
+
             return false;
         }
         for (int i = 0; i < inputNumber.length() - 1; i++) {
             for (int j = i + 1; j < inputNumber.length(); j++) {
                 if (inputNumber.charAt(i) == inputNumber.charAt(j)) {
+                    System.out.println("Моля въведете число с неповтарящи се цифри.");
+
                     return false;
                 }
             }
@@ -42,6 +55,7 @@ public class Main {
             inputNumberList.add(inputNumber % 10);
             inputNumber /= 10;
         } while (inputNumber > 0);
+
         Collections.reverse(inputNumberList);
 
         return inputNumberList;
@@ -71,30 +85,20 @@ public class Main {
         return countBulls;
     }
 
-    public static boolean printOutput(int inputNumber, ArrayList<Integer> randomNumber) {
-        int cows = countCows(convertIntToArrayList(inputNumber), randomNumber);
-        int bulls = countBulls(convertIntToArrayList(inputNumber), randomNumber);
-        int digits = randomNumber.size();
-        if (bulls == digits) {
-            System.out.println("Честито! Познахте числото.");
-            return true;
-        } else {
-            if (cows != 0) {
-                System.out.print(cows + (cows == 1 ? " крава" : " крави"));
-            }
-            if (cows != 0 && bulls != 0) {
-                System.out.print(" и ");
-            }
-            if (cows == 0 && bulls == 0) {
-                System.out.print("Няма съвпадения.");
-            }
-            if (bulls != 0) {
-                System.out.print(bulls + (bulls == 1 ? " бик" : " бика"));
-            }
-            System.out.println();
+    public static void printOutput(int cows, int bulls) {
+        if (cows != 0) {
+            System.out.print(cows + (cows == 1 ? " крава" : " крави"));
         }
-
-        return false;
+        if (cows != 0 && bulls != 0) {
+            System.out.print(" и ");
+        }
+        if (cows == 0 && bulls == 0) {
+            System.out.print("Няма съвпадения.");
+        }
+        if (bulls != 0) {
+            System.out.print(bulls + (bulls == 1 ? " бик" : " бика"));
+        }
+        System.out.println();
     }
 
     public static void printArrayList(ArrayList<Integer> list) {
@@ -106,20 +110,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int digits = 4;
-        ArrayList<Integer> randomNumber = generateRandomNumber(digits);
-        printArrayList(randomNumber);
+        ArrayList<Integer> randomNumberList = generateRandomNumber(digits);
+        printArrayList(randomNumberList);
         System.out.println();
-        String input;
-        boolean end = false;
+        int bulls = 0;
         do {
             System.out.print("Въведете число: ");
-            input = scanner.nextLine();
+            String input = scanner.nextLine();
             if (validateInput(input, digits)) {
                 int inputNumber = Integer.parseInt(input);
-                end = printOutput(inputNumber, randomNumber);
-            } else {
-                System.out.println("Въведете валидно число!");
+                ArrayList<Integer> inputNumberList = convertIntToArrayList(inputNumber);
+                int cows = countCows(inputNumberList, randomNumberList);
+                bulls = countBulls(inputNumberList, randomNumberList);
+                printOutput(cows, bulls);
             }
-        } while (!end);
+        } while (bulls != digits);
     }
 }
